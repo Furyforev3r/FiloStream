a1d52ed2388842fbdd8a995697c79821b231953f
FuryForev3r <messiasfuryforever@gmail.com>
2024-10-25 22:54:34 -0300

    feat: Dashboard carousel added

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -86,6 +86,7 @@
                 toast.success("Video added successfully!", {
                     style: 'background: #222; color: #fff;'
                 })
+                await fetchUserVideos(userInfo.uid)
             } else {
                 formError = response.data.error || "Failed to add video"
             }
